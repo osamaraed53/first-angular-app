23e44d526eaab3d750fbb22f736b7d132134e785
Osama Raed <osamaraed53@gmail.com>
2026-06-19 14:20:44 +0300

    inititCommit

---
+++ first-angular-app/src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import {HomeComponent }  from './home/home.component'
+
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,HomeComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class AppComponent {
+  title = 'first-angular-app';
+}
